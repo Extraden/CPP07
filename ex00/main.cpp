15da04f98c4e0ba21769ee46fd6a760fd8a3c713
dsemenov <dsemenov@student.42lyon.fr>
2026-09-12 17:04:32 +0200

    feat: implement template in ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include "whatever.hpp"
+
 int main(void)
 {
 	int a = 2;
@@ -5,18 +8,18 @@ int main(void)
 
   (void)a;
   (void)b;
-	// ::swap(a, b);
-	//
-	// std::cout << "a = " << a << ", b = " << b << std::endl;
-	// std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-	// std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-	//
-	// std::string c = "chaine1";
-	// std::string d = "chaine2";
-	//
-	// ::swap(c, d);
-	// std::cout << "c = " << c << ", d = " << d << std::endl;
-	// std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-	// std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	::swap(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 	return 0;
 }
