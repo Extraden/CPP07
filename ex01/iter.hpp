b4361e5cadc28ef7c409c1ca1849989cd9c6b864
dsemenov <dsemenov@student.42lyon.fr>
2026-09-12 20:37:13 +0200

    implement iter function template

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,10 +1,15 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
 #include <cstddef>
 
-template <typename T>
-void  iter(T* arr, const unsigned long size, void (func)())
+template <typename T, typename F>
+void  iter(T* arr, const std::size_t size, F func)
 {
   for (std::size_t i = 0; i < size; ++i)
   {
     func(arr[i]);
   }
 }
+
+#endif
